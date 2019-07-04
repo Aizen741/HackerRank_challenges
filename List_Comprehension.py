e04e19965359f30e5172f09b98b2e362532f15a0
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-04 15:13:48 +0530

    L_c

---
+++ List_Comprehension.py
@@ -0,0 +1,13 @@
+
+
+x = int(input())
+y = int(input())
+z = int(input())
+
+N = int(input())
+
+
+mylist= [[i,j,k] for i in range (x+1) for j in range (y+1) for k in range (z+1) if i+j+k != N]
+
+print(mylist)
+
