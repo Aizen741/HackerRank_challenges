c70f17de7c08f9746933f5a80fa304572ef4bafa
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-17 20:46:41 +0530

    Nested List1

--- Nested_Lists.py
+++ Nested_Lists.py
@@ -1,13 +1,30 @@
+from typing import Dict, Any
+
 Number_of_students = int(input())
 d = {}
 def Students():
+    global Name
+    global Marks
     for i in range(Number_of_students):
         Name = str(input())
         Marks = (input())
         d[Name] = float(Marks)
-    dictionary= (d)
-    dictionary = sorted(dictionary.keys())
-    return dictionary[-2]
+    dictionary: dict[Name, Marks] = (d)
+
+    value = sorted(dictionary , key=dictionary.__getitem__)
+    for i in value:
+        if value.key[1] == value.key[2]:
+            return value[1]
+
+        else:
+            return i in value[1] , value[2]
+
+
+
+
+
+    return sortedvalue
+
 print(Students())
 
 
