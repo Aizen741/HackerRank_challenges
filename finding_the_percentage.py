a1e00fd9c6948f65e0010d462463aeb13d8ba6b3
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-24 02:37:29 +0530

    Challenge

--- finding_the_percentage.py
+++
@@ -1,9 +0,0 @@
-number_of_students = int(input())
-results = {}
-
-for i in range(number_of_students):
-    a = input().split(' ')
-    results[a[0]] = [float(x) for x in a[1:]]
-student = input()
-
-print("%.2f" %(sum(results[student])/len(results[student])))
