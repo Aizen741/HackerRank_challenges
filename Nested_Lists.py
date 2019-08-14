c66da14838ef030be67e1b4761f7b806bb9cee6a
Aizen741 <rahulsivadas2009@gmail.com>
2019-08-14 07:44:16 +0530

    ...

--- Nested_Lists.py
+++ Nested_Lists.py
@@ -1,1 +1,9 @@
+N = int(input("number of students: "))
 
+
+def students():
+    for i in range(1,N+1):
+        name = str(input("Name: "))
+        marks = input("Marks: ")
+    return[[name , marks]]
+print(students())
