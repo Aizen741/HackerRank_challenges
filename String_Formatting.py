89c18247c744deda02f80fefb5d41122c0987be6
Aizen741 <rahulsivadas2009@gmail.com>
2020-07-07 00:35:28 +0530

    challenges

---
+++ String_Formatting.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    while number in range(1,number):
+
+        return ((number))
+
+if __name__ == '__main__':
+    n = int(input())
+    print(print_formatted(n))
---
+++ itertools.py
@@ -0,0 +1,9 @@
+
+from itertools import product
+
+a = map(int,input().split())
+b = map(int,input().split())
+x = [a,b]
+
+print(str([ each for each in list(product(*x)) ])
+[1:-1].replace(", (", " ("))
